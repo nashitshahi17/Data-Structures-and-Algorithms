9c4bf4e0c9966e80f3fb86766ce61e3168075da0
nashithshahi17 <nashitshahi17@gmail.com>
2026-04-30 19:33:21 +0530

    day11 commit

--- Binary Trees/flattenbinarytreetoll.java
+++ Binary Trees/flattenbinarytreetoll.java
@@ -25,3 +25,20 @@ public class flattenbinarytreetoll {
         return;
     }
 }
+
+// Morris Traversal Space Complexity O(1)
+// public void flatten(TreeNode root) {
+//         TreeNode curr = root;
+//         while(curr!=null){
+//             if(curr.left!=null){
+//                 TreeNode pred = curr.left;
+//                 while(pred.right!=null){
+//                     pred = pred.right;
+//                 }
+//                 pred.right = curr.right;
+//                 curr.right = curr.left;
+//                 curr.left = null;
+//             }
+//             curr = curr.right;
+//         }
+//     }

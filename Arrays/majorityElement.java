de8928ec8dda2d11af8ec1520e5c4510ecd93ae0
nashitshahi17 <nashitshahi17@gmail.com>
2026-07-16 20:23:06 +0530

    majorityElement

---
+++ Arrays/majorityElement.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class majorityElement{
+    public static int majele(int[] arr){
+        int n = arr.length;
+        int candidate = 0;
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(count==0){
+                candidate = arr[i];
+            }
+            if(arr[i]==candidate){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return candidate;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.print(majele(arr));
+    }
+}

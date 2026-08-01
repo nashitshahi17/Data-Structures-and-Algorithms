7f7d35c5fc7e145c296a430d2e331a6e3745c7b4
nashitshahi17 <nashitshahi17@gmail.com>
2026-08-01 14:25:12 +0530

    column wise snake pattern

---
+++ Arrays/snakepatterncolumwise.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class snakepatterncolumwise {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int[][] arr = new int[r][c];
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        sc.close();
+        for(int j=0;j<arr[0].length;j++){
+            if(j%2==0){
+                for(int i=0;i<arr.length;i++){
+                    System.out.print(arr[i][j]+" ");
+                }
+                System.out.println();
+            }else{
+                for(int i=arr.length-1;i>=0;i--){
+                    System.out.print(arr[i][j]+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
